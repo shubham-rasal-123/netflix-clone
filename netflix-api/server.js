bcfcadf47444a8838b69ecdbc02d23681a84a566
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2023-10-13 17:45:57 +0530

    added netflix-api folders and files

---
+++ netflix-api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successfull");
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, console.log("server Started..."));
