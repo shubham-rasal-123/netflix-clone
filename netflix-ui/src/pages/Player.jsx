7b2caf8c1a82c37aded38be8f120b1b306c9a7a1
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-07-02 13:28:13 +0530

    modified player jsx file

--- netflix-ui/src/pages/Player.jsx
+++ netflix-ui/src/pages/Player.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { BsArrowLeft } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import video from "../assets/video.mp4";
+
 export default function Player() {
   const navigate = useNavigate();
 
