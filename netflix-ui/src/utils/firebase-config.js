63dfb0799ab54304d96d6b60b1babb618d86ebf9
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-07-02 13:30:18 +0530

    modified firebase-config js file

--- netflix-ui/src/utils/firebase-config.js
+++ netflix-ui/src/utils/firebase-config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCEXtm-xO11pjD4TcyKj-t_FkEk8hA9v2A",
