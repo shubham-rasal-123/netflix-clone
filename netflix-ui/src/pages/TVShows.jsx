e4bd94ad5ba21a6c93d457365cfcdbe86f06a519
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-07-02 13:29:26 +0530

    modified tvshows jsx file

--- netflix-ui/src/pages/TVShows.jsx
+++ netflix-ui/src/pages/TVShows.jsx
@@ -1,53 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { fetchMovies, getGenres } from "../store";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import styled from "styled-components";
+// eslint-disable-next-line
+import CardSlider from "../components/CardSlider";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
-import NotAvailable from "../components/NotAvailable";
 import SelectGenre from "../components/SelectGenre";
 
 export default function TVShows() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
   const movies = useSelector((state) => state.netflix.movies);
   const genres = useSelector((state) => state.netflix.genres);
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  // eslint-disable-next-line
+  const dataLoading = useSelector((state) => state.netflix.dataLoading);
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getGenres());
+    if (!genres.length) dispatch(getGenres());
     // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
     if (genresLoaded) {
-      dispatch(fetchMovies({ type: "tv" }));
+      dispatch(fetchMovies({ genres, type: "tv" }));
     }
     // eslint-disable-next-line
   }, [genresLoaded]);
 
+  // eslint-disable-next-line
+  const [user, setUser] = useState(undefined);
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) setUser(currentUser.uid);
+    else navigate("/login");
+  });
+
   window.onscroll = () => {
-    setIsScrolled(window.scrollY === 0 ? false : true);
+    setIsScrolled(window.scrollX === 0 ? false : true);
     return () => (window.onscroll = null);
   };
 
-  onAuthStateChanged(firebaseAuth, (currentUser) => {
-    // if (currentUser) navigate("/");
-  });
-
   return (
     <Container>
-      <div className="navbar">
-        <Navbar isScrolled={isScrolled} />
-      </div>
+      <Navbar isScrolled={isScrolled} />
       <div className="data">
         <SelectGenre genres={genres} type="tv" />
-        {movies.length ? <Slider movies={movies} /> : <NotAvailable />}
+        {movies?.length ? (
+          <>
+            {" "}
+            <Slider movies={movies} />{" "}
+          </>
+        ) : (
+          <h1 className="not-available">
+            No TV Shows avaialble for the selected genre. Please select a
+            different genre.
+          </h1>
+        )}
       </div>
     </Container>
   );
