54cb0abb5a20be2f94692596e0d93cd21821fe4e
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-07-02 13:23:16 +0530

    modified app jsx file

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -6,18 +6,19 @@ import Netflix from "./pages/Netflix";
 import Player from "./pages/Player";
 import Movies from "./pages/Movies";
 import TVShows from "./pages/TVShows";
-import UserLiked from "./pages/UserLiked";
+import UserListedMovies from "./pages/UserListedMovies";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/Login" element={<Login />} />
-        <Route exact path="/Signup" element={<Signup />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
         <Route exact path="player" element={<Player />} />
-        <Route exact path="/movies" element={<Movies />} />
         <Route exact path="/tv" element={<TVShows />} />
-        <Route exact path="/mylist" element={<UserLiked />} />
+        <Route exact path="/movies" element={<Movies />} />
+        <Route exact path="/new" element={<Player />} />
+        <Route exact path="/mylist" element={<UserListedMovies />} />
         <Route exact path="/" element={<Netflix />} />
       </Routes>
     </BrowserRouter>
